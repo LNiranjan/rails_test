aab8cc347384664624b61d17c8ec381f8663bb02
LNiranjan <niranjan.kvana@gmail.com>
2017-04-03 16:57:32 +0530

    test case passed

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 require "zombie"
 
 describe Zombie do
-  it "is named Dash" do
+  it "is named Ash" do
     zombie = Zombie.new
-    zombie.name.should == 'Dash'
+    zombie.name.should == 'Ash'
   end
 end
