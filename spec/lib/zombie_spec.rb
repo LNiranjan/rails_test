36a9af1fa3ec6fdaf9be37db805638c804691eb4
LNiranjan <niranjan.kvana@gmail.com>
2017-04-03 16:10:46 +0530

    changes made

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 require "zombie"
 
 describe Zombie do
-  it "is named Ash" do
+  it "is named Dash" do
     zombie = Zombie.new
-    zombie.name.should == 'Ash'
+    zombie.name.should == 'Dash'
   end
 end
